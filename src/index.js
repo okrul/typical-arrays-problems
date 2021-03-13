902147e3be8fb14b66348bf0a237bad4b5c03eaf
Aliaksandr Okrul <okrul@list.ru>
2021-03-13 15:51:50 +0300

    feat: implement min, max and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array == undefined || array.length == 0)
+    {
+      return 0;
+    }
+  let minz = array[0];
+    array.forEach(element => {
+
+      minz = element < minz ? element : minz;
+      
+    });
+  
+  return minz;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == undefined || array.length == 0)
+  {
+    return 0;
+  }
+let maxz = array[0];
+  array.forEach(element => {
+
+    maxz = element > maxz ? element : maxz;
+    
+  });
+
+return maxz;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == undefined || array.length == 0)
+  {
+    return 0;
+  }
+  let sumz = 0;
+  array.forEach(element => {
+
+    sumz = sumz + element;
+    
+  });
+
+  let avgz = sumz/array.length;
+return avgz;
 }
